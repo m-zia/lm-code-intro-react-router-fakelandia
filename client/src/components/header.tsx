24173c4ffd68ba370b6494204f615dd72863409a
M Zia <murtaza_zia@hotmail.co.uk>
2023-09-13 18:24:52 +0100

    added CSS to header and nav; completed activity 1

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,24 +1,27 @@
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import './header.css';
 
 function Header() {
-    return (
-        <>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/misdemeanours">Misdemeanours</Link>
-                    </li>
-                    <li>
-                        <Link to="/confession">Confession</Link>
-                    </li>
-                </ul>
-            </nav>
-        </>
-
-    )
+  return (
+    <>
+      <nav className="navbar">
+        <div className="nav-container">
+          <ul>
+            <li className="logo nav-link">FAKELANDIA</li>
+            <li>
+              <Link to="/" className="nav-link">Home</Link>
+            </li>
+            <li>
+              <Link to="/misdemeanours" className="nav-link">Misdemeanours</Link>
+            </li>
+            <li>
+              <Link to="/confession" className="nav-link">Confess To Us</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
 }
 
-export default Header
+export default Header;
--- client/src/components/home.tsx
+++ client/src/components/home.tsx
@@ -1,6 +1,15 @@
 
 function Home() {
-  return <h2>Home</h2>;
+    return (
+
+        <>
+            <h2>Welcome to the home of the justice department of Fakelandia</h2>
+
+            <h2>Here you can browse a list of recent misdemeanours commited by our citizens, or you can confess to your own misdemeanour  </h2>
+
+        </>
+    )
+
 }
 
 export default Home;
